0880e7a5484b823bb9c5e12b0fcadc20e1a7062b
shjy2008 <shjy2011@163.com>
2025-05-04 14:35:12 +1200

    config args

--- classifier.py
+++ classifier.py
@@ -342,7 +342,7 @@ def get_args():
     parser.add_argument("--hidden_dropout_prob", type=float, default=0.3)
     parser.add_argument("--lr", type=float, help="learning rate, default lr for 'pretrain': 1e-3, 'finetune': 1e-5",
                         default=1e-5)
-    parser.add_argument("--POS_tag_enabled", type=bool, default=True)
+    parser.add_argument("--POS_tag_enabled", type=int, default=1)
 
     args = parser.parse_args()
     print(f"args: {vars(args)}")
